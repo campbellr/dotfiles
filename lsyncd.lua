c316cd5a9cda752dca14af3c0a5eebce9739cfd1
Ryan Campbell <Ryan.Campbell@emc.com>
2014-12-04 13:58:57 -0700

    add lsyncd config file

---
+++ lsyncd.lua
@@ -0,0 +1,11 @@
+settings {
+    logfile = "/tmp/lsyncd.log",
+    statusFile = "/tmp/lsyncd.status",
+}
+
+sync {
+    default.rsync,
+    source = "/localhome/campbr9",
+    target = "/home/campbr9",
+    delete = false,
+}
